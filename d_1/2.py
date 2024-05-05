2234e82ca87adb025f83e2c2d0649be6e80d0d1d
Marcin Kaliszewski <hrabiadetrittus@gmail.com>
2024-05-05 22:32:59 +0200

    Bug fixed - there was some inaccuracy in input file

--- d_1/2.py
+++ d_1/2.py
@@ -6,11 +6,7 @@ def main():
     lines = file1.readlines()
     sum = 0
 
-    count = 0
     for line in lines:
-        count = count + 1
-        if count<20:
-            print(line)
         line = line.replace("zero","z1o")
         line = line.replace("one","o1e")
         line = line.replace("two","t2o")
@@ -22,9 +18,6 @@ def main():
         line = line.replace("eight","e8h")
         line = line.replace("nine","n9e")
         x = re.findall(r"\d",line)
-        if count<20:
-            print(line)
-            print(x)
         if len(x) == 1:
             sum = sum + int(x[0])*10 + int(x[0])
         elif len(x) > 1:
