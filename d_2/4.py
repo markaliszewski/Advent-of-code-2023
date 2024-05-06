d0ef5e05aa705f1ab0c6f31d74d9942de3a06565
Marcin Kaliszewski <hrabiadetrittus@gmail.com>
2024-05-06 15:22:01 +0200

    Solved problem 4. Mateusz's solution added to problem 2.

---
+++ d_2/4.py
@@ -0,0 +1,37 @@
+import re
+
+def main():
+    file1 = open("input.txt", "r")
+
+    lines = file1.readlines()
+    result = 0
+    
+    for line in lines:
+        fail = False
+        max_green = 0
+        max_red = 0
+        max_blue = 0
+        entries = re.split(':|;|,',line)
+        for entry in entries:
+            key = ''.join(re.findall(r'[a-zA-Z]*',entry)) 
+            value = re.findall(r'\d+',entry)
+            match key:
+                case 'red':
+                    if int(value[0])>max_red:
+                        max_red = int(value[0])
+                case 'blue':
+                    if int(value[0])>max_blue:
+                        max_blue = int(value[0])
+                case 'green':
+                    if int(value[0])>max_green:
+                        max_green = int(value[0])
+                        
+        result = result + max_green*max_red*max_blue
+            
+    print(result)
+
+    file1.close()
+
+if __name__ == "__main__":
+    main()
+
