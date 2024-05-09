1fd5ae31006141f617bcc7f4dd134953b79c209d
Marcin Kaliszewski <hrabiadetrittus@gmail.com>
2024-05-09 13:02:00 +0200

    d_4_p_2 solved

--- d_4/d_4_p_2.py
+++ d_4/d_4_p_2.py
@@ -2,26 +2,25 @@
 import re
 from os.path import dirname, join
 
+import numpy as np
+
+
 def points_of_game(line: str) -> int:
     """
-    Returns the number of points gained
+    Returns the number of new cards gained
     
     :param line: line to scan
-    :return: points gained from game included in line
+    :return: number of new cards gained from game included in line
     """
     tmp = re.split(r'[*:*|]', line)
     left_set = list(map(int, re.findall(r'\d+', tmp[1])))
     right_set = list(map(int, re.findall(r'\d+', tmp[2])))
     number_of_hits = len(set(left_set) & set(right_set))
-    a = pow(2, number_of_hits - 1)
-    if a < 1:
-        return 0
-    return int(a)
+    return int(number_of_hits)
 
 def main() -> None:
     """
-    Prints the sum of the power of the minimum set of cubes that must have been present
-    in each game included in input.txt according to d_2_p_2_task.txt
+    Prints the sum of all cards obtained and won from input.txt according to d_4_p_2_task.txt
     """
     current_dir = dirname(__file__)
     file_path = join(current_dir, "./input.txt")
@@ -29,11 +28,21 @@ def main() -> None:
     with open(file_path, "r", encoding="UTF-8") as file1:
         lines = file1.readlines()
 
-    sum_of_points = 0
-    for line in lines:
-        sum_of_points += points_of_game(line)
+    list_of_card_scores = np.zeros(len(lines), dtype=int)
+    number_of_cards = np.ones(len(lines), dtype=int)
 
-    print(sum_of_points)
+    for i, line in enumerate(lines):
+        list_of_card_scores[i] = points_of_game(line)
+
+
+    for i, card_score in enumerate(list_of_card_scores):
+        for j in range(1,card_score+1):
+            try:
+                number_of_cards[i+j] += 1*number_of_cards[i]
+            except IndexError:
+                pass
+
+    print(sum(number_of_cards))
 
 if __name__ == "__main__":
     main()
